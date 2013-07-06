8a8a81f69519330288335b788eef5536e3673ee0
Rufus Pollock <rufus.pollock@okfn.org>
2013-07-06 16:13:23 +0100

    [all][m]: initial extract from recline.
    * only tweak is minor one to test to make tests work without recline core

---
+++ README.md
@@ -0,0 +1,5 @@
+Pure simple javascript CSV parser focused on the browser. Originally built as
+part of [ReclineJS][].
+
+[ReclineJS]: http://okfnlabs.org/recline/
+
---
+++ test/tests.js
@@ -0,0 +1,115 @@
+(function ($) {
+module("Backend Local CSV");
+
+test("parseCSV", function() {
+  var csv = '"Jones, Jay",10\n' +
+  '"Xyz ""ABC"" O\'Brien",11:35\n' +
+  '"Other, AN",12:35\n';
+
+  var array = recline.Backend.CSV.parseCSV(csv);
+  var exp = [
+    ['Jones, Jay', 10],
+    ['Xyz "ABC" O\'Brien', '11:35' ],
+    ['Other, AN', '12:35' ]
+  ];
+  deepEqual(exp, array);
+
+  var csv = '"Jones, Jay", 10\n' +
+  '"Xyz ""ABC"" O\'Brien", 11:35\n' +
+  '"Other, AN", 12:35\n';
+  var array = recline.Backend.CSV.parseCSV(csv, {trim : true});
+  deepEqual(exp, array);
+
+  var csv = 'Name, Value\n' +
+  '"Jones, Jay", 10\n' +
+  '"Xyz ""ABC"" O\'Brien", 11:35\n' +
+  '"Other, AN", 12:35\n';
+  var dataset = {
+    data: csv,
+    backend: 'csv'
+  };
+  // strictly this is asynchronous
+  recline.Backend.CSV.fetch(dataset).done(function(dataset) {
+    equal(dataset.records.length, 3);
+    var row = dataset.records[0];
+    deepEqual(dataset.fields, ['Name', 'Value']);
+    deepEqual(row, ['Jones, Jay', 10]);
+  });
+});
+
+test("parseCSV - semicolon", function() {
+  var csv = '"Jones; Jay";10\n' +
+  '"Xyz ""ABC"" O\'Brien";11:35\n' +
+  '"Other; AN";12:35\n';
+
+  var array = recline.Backend.CSV.parseCSV(csv, {delimiter : ';'});
+  var exp = [
+    ['Jones; Jay', 10],
+    ['Xyz "ABC" O\'Brien', '11:35' ],
+    ['Other; AN', '12:35' ]
+  ];
+  deepEqual(exp, array);
+
+});
+
+test("parseCSV - quotechar", function() {
+  var csv = "'Jones, Jay',10\n" +
+  "'Xyz \"ABC\" O''Brien',11:35\n" +
+  "'Other; AN',12:35\n";
+
+  var array = recline.Backend.CSV.parseCSV(csv, {quotechar:"'"});
+  var exp = [
+    ["Jones, Jay", 10],
+    ["Xyz \"ABC\" O'Brien", "11:35" ],
+    ["Other; AN", "12:35" ]
+  ];
+  deepEqual(exp, array);
+
+});
+
+test("parseCSV skipInitialRows", function() {
+  var csv = '"Jones, Jay",10\n' +
+  '"Xyz ""ABC"" O\'Brien",11:35\n' +
+  '"Other, AN",12:35\n';
+
+  var array = recline.Backend.CSV.parseCSV(csv, {skipInitialRows: 1});
+  var exp = [
+    ['Xyz "ABC" O\'Brien', '11:35' ],
+    ['Other, AN', '12:35' ]
+  ];
+  deepEqual(exp, array);
+});
+
+test("serializeCSV - Array", function() {
+  var csv = [
+    ['Jones, Jay', 10],
+    ['Xyz "ABC" O\'Brien', '11:35' ],
+    ['Other, AN', '12:35' ]
+  ];
+
+  var array = recline.Backend.CSV.serializeCSV(csv);
+  var exp = '"Jones, Jay",10\n' +
+  '"Xyz \"ABC\" O\'Brien",11:35\n' +
+  '"Other, AN",12:35\n';
+  deepEqual(array, exp);
+});
+
+test("serializeCSV - Object", function() {
+  var indata = {
+    fields: [ {id: 'name'}, {id: 'number'}],
+    records: [
+      {name: 'Jones, Jay', number: 10},
+      {name: 'Xyz "ABC" O\'Brien', number: '11:35' },
+      {name: 'Other, AN', number: '12:35' }
+    ]
+  };
+
+  var array = recline.Backend.CSV.serializeCSV(indata);
+  var exp = 'name,number\n' +
+  '"Jones, Jay",10\n' +
+  '"Xyz \"ABC\" O\'Brien",11:35\n' +
+  '"Other, AN",12:35\n';
+  deepEqual(array, exp);
+});
+
+})(this.jQuery);
